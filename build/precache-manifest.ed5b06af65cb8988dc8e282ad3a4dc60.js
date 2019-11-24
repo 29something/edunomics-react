9b6c1d215b6d2d810b02e5f9ea77fd5d3e32992c
29something <agnihotri.chitransh@gmail.com>
2019-11-24 17:12:54 +0530

    3rd commit

---
+++ build/precache-manifest.ed5b06af65cb8988dc8e282ad3a4dc60.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "8f3d979cd8245be5a988",
+    "url": "/static/css/main.7f984ff2.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "610cff81bbdb5a3814814bb93b558273",
+    "url": "/static/media/29.610cff81.png"
+  },
+  {
+    "revision": "d4e56e9aafe6176170fa",
+    "url": "/static/js/2.3d849c05.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "8f3d979cd8245be5a988",
+    "url": "/static/js/main.c1681b53.chunk.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "19f8c8179380317c956530da9d1969dd",
+    "url": "/static/media/eliteadmin-small-text.19f8c817.png"
+  },
+  {
+    "revision": "8cccc71bedb39a2e967fdb1e76532b39",
+    "url": "/static/media/1.8cccc71b.jpg"
+  },
+  {
+    "revision": "92fb17ecdfda04c14deb22b1175e31cf",
+    "url": "/static/media/2.92fb17ec.jpg"
+  },
+  {
+    "revision": "7896245001dbc6883d17f7b6e412acac",
+    "url": "/static/media/3.78962450.jpg"
+  },
+  {
+    "revision": "7eef6399163e94d847992e8f7f9797bb",
+    "url": "/static/media/4.7eef6399.jpg"
+  },
+  {
+    "revision": "d4e56e9aafe6176170fa",
+    "url": "/static/css/2.6ef85f8a.chunk.css"
+  },
+  {
+    "revision": "1d10a4930b4ab2c1d7c6cfb07b11c320",
+    "url": "/index.html"
+  }
+];
--- src/services/url.js
+++ src/services/url.js
@@ -4,6 +4,3 @@
 // export const API_URL = '/api/';
 //development
 export const API_URL = "http://34.93.244.58/api/";
-//  export const API_URL = 'https://qsys1.herokuapp.com/api/'
-//production
-//export const API_URL = 'http://35.200.214.50/api/';
